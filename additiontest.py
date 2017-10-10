21fc5811fa17618610856b220452ffbed585a6b6
Eric Kiplagat <ekiplagat@SamaDc.corp>
2017-10-10 11:09:13 +0300

    added 3rd test

--- additiontest.py
+++ additiontest.py
@@ -5,5 +5,7 @@ class testsum(unittest.TestCase):
 	     self.assertRaises(TypeError)
      def test_answer_is_correct(self):
 	     self.assertEqual(addition(2,3),5)
+     def test_solution_is_not_product(self):
+	     self.assertRaises(TypeError,addition(2,3),6)
 if __name__ == "__main__":
  unittest.main()
